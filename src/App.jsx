8557af664950cdb78b1dabd450e677cabf67a017
shubhamdube671 <shubhamdubey671@gmail.com>
2025-05-23 01:48:07 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+    return (
+        <>
+            <div className="container" style={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                width: "100vw"
+            }}>
+                <h3>Aws is running</h3>
+
+            </div>
+        </>
+    )
+}
